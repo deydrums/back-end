051a60929ff9b8ddd910b11bd3405d807123e07d
deydrums <dagarcia100@gmail.com>
2021-08-20 15:18:21 -0600

    Proyectos migraciones

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Project;
+
+class ProjectFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Project::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'desc' => $this->faker->paragraphs(1, true),
+            'image' => null,
+            'date' => now(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CategorySeeder::class,
-            EntrySeeder::class,       
+            EntrySeeder::class,
+            ProjectSeeder::class,       
         ]);
     }
 }
